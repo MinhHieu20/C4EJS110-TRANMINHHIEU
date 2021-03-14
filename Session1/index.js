9a12d47386ce54be92ec74bd62267f51a5f1af93
MinhHieu20 <tranhieuv123@gmail.com>
2021-03-14 17:03:21 +0700

    bai buoi 2

---
+++ Session1/index.js
@@ -0,0 +1,70 @@
+//bai1
+{
+   let x1 ="-lỗi thường khi đặt tên biến:một khai báo lặp lại cùng 1 tên biến;tên danh riêng;một nhiệm vụ không có use strict;bắt đầu bằng số;chứa các ký tự đặc biệt ngoại trừ '_','$';trùng với các từ khóa";
+   console.log(x1);
+   //vidu  
+   //let name="your";
+   //let name="you";
+   //let let =5;
+   
+       let x2 ="-toán tử 'typeof' sẽ giúp chúng ta kiểm tra kiểu dữ liệu của một kiểu dữ liệu bất kỳ";
+       console.log(x2);
+   
+}
+//bai2
+{
+    let x3 ="1-b,2-a,3-e,4-c";
+    console.log(x3);
+}
+//bai3
+{
+    let message = "Coding is great";
+    
+        let studentCount = "0";
+        console.log(studentCount);
+    
+    console.log(message);
+}
+//bai4
+{
+    let message1 = "CODING MIGHT NOT BE EASY, BUT STILL GREAT";
+    console.log(message1);
+    
+        let studentCount1 = "19";
+        console.log(studentCount1);
+    
+     console.log(message1.toLocaleLowerCase());  
+ console.log(studentCount1 + 1);
+}
+
+//bai5
+    let hieu ='you look beautiful today';
+    alert(hieu); 
+//bai6
+    let trhieu =prompt(" Hi there,your name please?");
+    console.log(hieu);
+//bai7
+    let fname =prompt('Enter your first name');
+    console.log(fname);
+    let lname =prompt('Enter your last name');
+    console.log(lname);
+    alert('Hi'+" "+(fname+" "+lname));
+
+//bai8
+    let dtv =prompt("Enter side leght of the spuare");
+    console.log(dtv*dtv);
+    alert(`dien tich hinh vuong ${dtv*dtv}`);
+
+//bai9
+    let dtvt =prompt("Enter the radius ò the circle");
+    console.log(dtvt*dtvt*3.14);
+    alert(`dien tich hinh tron ${dtvt*dtvt*3.14}`);
+
+//bai10
+   let doc =prompt("Enter the temperature in celsius");
+   console.log((doc*1.8) + 32);
+   alert(`doi thanh: ${doc+"C="+((doc*1.8) + 32)+"F"}`);
+
+
+
+
