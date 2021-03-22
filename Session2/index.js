9f4552b0899c6b783224c4c2bd2ff5de6e73ac3f
MinhHieu20 <tranhieuv123@gmail.com>
2021-03-22 16:18:51 +0700

    homework

--- Session2/index.js
+++ Session2/index.js
@@ -1,155 +1,164 @@
- //cau1
- {
-     let a ="let, var, const là các từ câu lệnh ở đầu mỗi biến trong js";
-    let b = "-var được khai báo lại trong phạm vi toàn cầu còn let không được khai báo lại trong phạm vi khối";
-    let c = " -let có thể gán lại giá trị còn cost thì không thể gán lại giá trị";
-    let d ="var, let, const được sử dụng trong các trường hợp khai báo một biến";
-    console.log(a)
-   console.log(b);
-    console.log(c);
-    console.log(d)
- }
- //cau2
- {
-     let a = " Boolean là kiểu toán tử logic và có hai giá trị là: True hoặc False";
-    console.log(a);
-    console.log('kết quả của Boolean là ra có/không; đung/sai ')
- }
+//  //cau1
+//  {
+//      let a ="let, var, const là các từ câu lệnh ở đầu mỗi biến trong js";
+//     let b = "- var được khai báo lại trong phạm vi toàn cầu còn let không được khai báo lại trong phạm vi khối";
+//     let c = " - let có thể gán lại giá trị còn cost thì không thể gán lại giá trị";
+//     let d =" var, let, const được sử dụng trong các trường hợp khai báo một biến";
+//     console.log(a)
+//    console.log(b);
+//     console.log(c);
+//     console.log(d)
+//  }
+//  //cau2
+//  {
+//      let a = " Boolean là kiểu toán tử logic và có hai giá trị là: True hoặc False";
+//     console.log(a);
+//     console.log('kết quả của Boolean là ra có/không; đung/sai ')
+//  }
  //cau3
  //ý a
- {
-    for(let x = 0;x < 7;x++)
-       console.log(x)
- }
- //ý b
-{
-   var n =Number
-    var n =prompt("Enter a number");
-   for(let x = 0;x < n;x++)
-       console.log(x);
- }
- //ý c
-  {
-    var str = Number;
-  var str =prompt("Enter n");
-    for(let x = 3;x < str;x++)
-      console.log(x);
- }
-//ý d
-  {
-    var c = String(Number);
-    var c =prompt("Enter c")
-   var n = String(Number);
-   var n =prompt("Enter n");
-  for(let x = c;x < n;x++)
-       console.log(x);
- }
- //ý e 
- {
-
- }
- //ý d
- {
-
- }
-//cau 4
-  {
-     var n = Number;
-    var gt =1 
-      var n = prompt("Enter a number")
-     for(let i = 1;i <=n;i++)
-       gt*=i;
-       alert(gt);
-
-// }
- //cau 5
- {
-     var age = prompt(" How old are you");
-     if(age<14){
-         alert("You are not old enough to view the content ")
-     }else alert("you are old enough to view the content")
- }
-//cau6
- {
-    var x =prompt("Enter a number") ;
-     if(x < 4) {
-         alert("Lower half of 9")
-     }else if(4.5 < x < 9){
-         alert("Hight half of 9")
-    }
-  }
-//cau 7
- {
-      x = Number;
-    var x =prompt("Enter x")
-     n = Number;
-    var n =prompt("Enter n")
-    if(n < x){
-        alert(x +""+ ("is hight half of") +""+ n)
-    }else if(x < n){
-        alert(n +" "+ ("is lower half of") +""+ x)
-    }
-}
-//cau 8
-{
-    x = Number;
-    var x = prompt(" ENter a number")
-    if(x % 2 == 0){
-        alert(x +" "+ (" is an even number"))
-    }else alert(x +" "+ (" is an odd number"))
-}
-//cau 9
-//ý a
-{
-    for(let h=1; h <=6;h++){
-        if(h<=3){
-            console.log('L')
-        }else console.log('H')
-    }
-}
-//ý b
-{
+//  {
+//     for(let x = 0;x < 7;x++)
+//        console.log(x)
+//  }
+//  //ý b
+// {
     
-    let x = Number(prompt('nhập số in ra L và H'));
-    let l,h;
-    for(l = 0;0 <= l & l < x;l++){
-        if(l< x/2){console.log('L');}
-     else  console.log('H')}
-}
-//ý c
-{
-    for (var lh = 1; lh <= 8; lh++) {
-    if (lh < 5) {console.log('0');}
-    else {console.log('1');}
-}
+//     var n =Number(prompt("Enter a number"));
+//    for(let x = 0;x < n;x++)
+//        console.log(x);
+//  }
+ //ý c
+//   {
+    
+//   var str =Number(prompt("Enter n"));
+//     for(let x = 3;x < str;x++)
+//       console.log(x);
+//  }
 //ý d
-{
-    var p = prompt('Viết số vào đây');
-for (let p1 = 1; p1 < p; p1++) {
-    if (p1 % 3 == 0) {
-        console.log("1");
-    }
-    else {
-        console.log("0");
-    }
-}
-}
-//cau 10
-{
-    w = Number;
-    var w = prompt(" Enter weight is kg")
-    h = Number;
-    var h = prompt("Enter height is cm")
-    var BMI=( w /((h*0.01) * (h*0.01)));
-    Math.ceil(BMI);
-    alert(Math.ceil(BMI));
-   if(BMI < 16){
-       alert("thieu can nang")
-   }else if(BMI< 18){
-       alert("thieu can")
-   }else if( BMI< 25){
-       alert("binh thuong")
-   }else if(BMI< 30){
-       alert("thua can")
-   }else alert("beo phi")
-}}}
+//   {
+//     var c = String(Number);
+//     var c =prompt("Enter c")
+//    var n = String(Number);
+//    var n =prompt("Enter n");
+//   for(let x = c;x < n;x++)
+//        console.log(x);
+//  }
+ //ý e 
+//  {
+//     var c =Number(prompt("Enter c"));
+//     var n =Number(prompt("Enter n"));
+//       for(let x = c;x <= n;x = x + 3)
+//            console.log(x);
+//  }
+// //  //ý d
+//  {
+//     var c =Number(prompt("Enter c"));
+//     var n =Number(prompt("Enter n"));
+//       for(let x = c;x <= n;x = x + s);
+//            console.log(x);
+//  }
+//cau 4
+//   {
+     
+//     var gt =1 
+//       var n =Number(prompt("Enter a number"))
+//      for(let i = 1;i <=n;i++)
+//        gt*=i;
+//        alert(gt);
+
+//  }
+ //cau 5
+//  {
+//      var age = prompt(" How old are you");
+//      if(age<14){
+//          alert("You are not old enough to view the content ")
+//      }else alert("you are old enough to view the content")
+//  }
+//cau6
+//  {
+//     var x =prompt("Enter a number") ;
+//      if(x < 4.5) {
+//          alert("Lower half of 9")
+//      }else if(4.5 < x < 9){
+//          alert("Hight half of 9")
+//     }
+//   }
+//cau 7
+//  {
+//       x = Number;
+//     var x =prompt("Enter x")
+//      n = Number;
+//     var n =prompt("Enter n")
+//     if(n < x){
+//         alert(x +""+ ("is hight half of") +""+ n)
+//     }else if(x < n){
+//         alert(n +" "+ ("is lower half of") +""+ x)
+//     }
+// }
+//cau 8
+// {
+//     x = Number;
+//     var x = prompt(" ENter a number")
+//     if(x % 2 == 0){
+//         alert(x +" "+ (" is an even number"))
+//     }else alert(x +" "+ (" is an odd number"))
+// }
+// //cau 9
+// //ý a
+// {
+//     for(let h=1; h <=6;h++){
+//         if(h<=3){
+//             console.log('L')
+//         }else console.log('H')
+//     }
+// }
+// //ý b
+// {
+    
+//     let x = Number(prompt('nhập số in ra L và H'));
+//     for( let l = 0;l < x;l++){
+//         if(l< x/2){console.log('L');}
+//      else  console.log('H')}
+// }
+// //ý c
+// {
+//     for (var lh = 1; lh <= 8; lh++) {
+//     if (lh < 5) {
+//         console.log('0');
+
+//     }else 
+//     {console.log('1');}
+//   }
+// }
+// //ý d
+// {
+//     var p = prompt('Viết số vào đây');
+// for (let p1 = 1; p1 < p; p1++) {
+//     if (p1 % 3 == 0) {
+//         console.log("1");
+//     }
+//     else {
+//         console.log("0");
+//     }
+// }
+// }
+// //cau 10
+// {
+//  
+//     var w = Number(prompt(" Enter weight is kg"))
+//    
+//     var h = Number(prompt("Enter height is cm"))
+//     var BMI=( w /((h*0.01) * (h*0.01)));
+//     Math.ceil(BMI);
+//     alert(Math.ceil(BMI));
+//    if(BMI < 16){
+//        alert("thieu can nang")
+//    }else if(BMI< 18){
+//        alert("thieu can")
+//    }else if( BMI< 25){
+//        alert("binh thuong")
+//    }else if(BMI< 30){
+//        alert("thua can")
+//    }else alert("beo phi")
+// }}}
--- Session3/index.js
+++ Session3/index.js
@@ -1,10 +1,12 @@
 //Init  
 {
      let menu =['tho san quai vat', 'Averger: End game', 'dao hai tac'];
- console.log(m/enu); }
+ console.log(menu); }
 
 //Create  
-{ let phim = prompt('enter phim moi'); 
+{ 
+    let menu =['tho san quai vat', 'Averger: End game', 'dao hai tac'];
+    phim = prompt('enter phim moi'); 
      menu.push(phim);
     console.log(menu); }
 
@@ -115,4 +117,157 @@
      alert('Your username is too long!');
  }
  }
+}
+
+//bai 1
+{
+    let a = 5;
+    let b = 6;
+    {let temp;// su dung bien tam thoi
+    temp = a;
+    a = b;
+    b = temp; 
+    console.log(a,b);}
+    {
+    let a1 = 1;
+    let b2 = 2;
+    a1 = a1 + b2;
+    b2 = a1 - b2;
+    a1 = a1 - b2;
+    console.log(a1,b2);}
+
+}
+
+//bai2
+{
+    const s = "Hello beauty there";
+    const r = s.split(" ");//tach tung mang 
+    const r1 = s.split("");//tach ca khoang trang
+    const r2 = s.split(" ",2);//tach o gioi han
+    const r3 = s.split("e");//lay chu cai lam dau phan tach
+    console.log(r,r1,r2,r3);
+}
+
+//bai3
+{
+    const a = [4, 5, 7, -8];
+    console.log(...a);
+}
+
+//bai4
+{
+    var Items = ['Jeans', 'T-shirf', 'Socks']
+    while(true){
+         question =prompt('Hi there, Wecome to shop admin panel.What do you want(c,r,u,d)?')
+        if(question == 'e'){
+            alert('This command is not supported');
+        } else if(question == 'r'){
+            for(i in Items){ 
+                console.log(`${Number(i)+1}. ${Items[i]}`)
+            }
+        }else if(question == 'c'){
+            let newItems = prompt('Enter the name of the new items')
+            Items.push(newItems);
+            alert('Done');
+        }else if(question == 'u'){
+            let n =prompt('Enter the position you want to update:')
+            Items[n]=prompt('Enter the new name:')
+            alert('Done');
+        }else if(question == 'd'){
+            let x = prompt('Enter the position you want to delete:')
+            Items.splice(x);
+            alert('Done'); 
+        }
+    }
+}
+
+//bai5
+{
+    let n =prompt('Enter a sequence of numbers,separated by commas,')
+    let array = n.split(",")
+    let sum = array.reduce((sum,value) => Number(sum) + Number(value));
+    alert(sum);
+}
+
+//bai 6
+{
+     n =prompt('Enter a sequence of numbers, separated by "," ')
+     var array = n.split(",");
+     let min = array.sort((min,value) => Number(min)-Number(value));
+    alert(min[0]); 
+}
+
+//bai7
+{
+    const arr =[3, 4, 6, -9, 10, -88, 2];
+    let x =Number(prompt("Enter a number element number "))
+    var search = arr.indexOf(x)
+    if(search != -1){
+        alert(`${x} is found in my array at index ${search}`);
+    } 
+    else 
+    alert(`${x} is not found in my array`)
+}
+ 
+//bai 8
+{
+    var arr = [ 5, 7, 300, 90, 24, 50, 75]
+    {
+    alert(`Hello,My name is Minh Hieu and here is my sheep sizes:${arr.join("  ")}`)
+}
+    
+    {
+        let max = arr.sort((max,value) => Number(value) - Number(max))
+    alert(`Now my biggest sheep sizes ${max[0]}, let's shave it`); 
+}
+    {
+    let search= arr.indexOf(arr.sort((max,value) => Number(value) - Number(max))[0])
+    arr[search] = 8
+    alert(`After shearing, here is my flock ${arr.join(" ")}`)
+}
+    for( let i = 1;i < arr.length;i++){
+        arr[i] += 50;
+        alert(`One month has, my sheeps have grown, here are their sizes ${arr.join(" ")}`)
+}
+    for(let i = 2;i <=3 ;i++){
+        let max = arr.sort((max,value) => Number(value) - Number(max))
+        alert(`Now my biggest sheep sizes ${max[0]}, let's shave it`); 
+        let search= arr.indexOf(max[0])
+        arr[search] = 8
+        alert(`After shearing, here is my flock ${arr.join(" ")}`)
+    for( let i = 0;i < arr.length;i++){
+        arr[i] += 50;
+        alert(`One month has, my sheeps have grown, here are their sizes ${arr.join(" ")}`) }
+}
+    let sum = 0;
+    for(i = 0 ; i < arr.length;i++){
+        arr[i] = Number(arr[i]);
+        sum +=arr[i];
+        console.log(`My flock has size in total: ${sum}`);
+        console.log(`I would get ${sum} * 2$ = ${sum*2} `)
+    }
+}
+
+//bai10
+{
+    let name =String(prompt('Enter a sequence of names'));
+    let aName = name.split(",");
+    for(i=0;i<aName.length;i++)
+    {
+        aName[i]= '<'+aName[i]+'>';
+    }
+    alert(name+'=>'+aName);
+}
+
+//bai11
+{
+    let number = prompt('Enter a sequence of number')
+    let anumber = number.split(",")
+    let add = [];
+    for(i = 0;i < number.length; i++){
+        if(anumber[i]%2 !=0){
+            add.push(anumber[i]);
+        }
+    } 
+    alert(number+'=>'+add);
 }
